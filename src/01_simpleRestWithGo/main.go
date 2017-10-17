785967816b92d98e6909d44d091aa0898166c208
Matthias Hertel <me@matthiashertel.eu>
2017-10-17 09:05:47 +0200

    split up - a little refactoring

--- src/01_simpleRestWithGo/main.go
+++ src/01_simpleRestWithGo/main.go
@@ -1,48 +1,13 @@
 package main
 
 import (
-    "fmt"
     "log"
     "net/http"
-    "time"
-    "encoding/json"
-    "github.com/gorilla/mux"
-
 )
 
 func main() {
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", Index)
-    router.HandleFunc("/todos", TodoIndex)
-    router.HandleFunc("/todos/{todoId}", TodoShow)
+
+    router := NewRouter()
 
     log.Fatal(http.ListenAndServe(":8080", router))
 }
-
-func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome")
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-    todos := Todos{
-        Todo{Name: "Learn GoLang Basics"},
-        Todo{Name: "Develop sth great"},
-    }
-
-    json.NewEncoder(w).Encode(todos)
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    todoId := vars["todoId"]
-    fmt.Fprintln(w, "Todo show:", todoId)
-}
-
-
-type Todo struct {
-    Name      string    `json:"name"`
-    Completed bool      `json:"completed"`
-    Due       time.Time `json:"due"`
-}
-
-type Todos []Todo
