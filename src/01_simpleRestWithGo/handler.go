6f38d355ea470f1d4cf4a6eac26943c3e441e6b6
Matthias Hertel <me@matthiashertel.eu>
2017-10-17 11:52:49 +0200

    no hardcoded data - the data comes from the persistence mock

--- src/01_simpleRestWithGo/handler.go
+++ src/01_simpleRestWithGo/handler.go
@@ -13,14 +13,8 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-    todos := Todos{
-        Todo{Name: "Learn GoLang Basics"},
-        Todo{Name: "Develop sth great"},
-    }
-
     w.Header().Set("Content-Type", "application/json; charset=UTF-8")
     w.WriteHeader(http.StatusOK)
-
     if err := json.NewEncoder(w).Encode(todos); err != nil {
         panic(err)
     }
