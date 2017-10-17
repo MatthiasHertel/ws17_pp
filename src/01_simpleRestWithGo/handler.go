95ef3102e7162ca6cb1d1e3ec33ce1ad241400df
Matthias Hertel <me@matthiashertel.eu>
2017-10-17 10:36:21 +0200

    expect json - set content typ + set status code explicitly

--- src/01_simpleRestWithGo/handler.go
+++ src/01_simpleRestWithGo/handler.go
@@ -18,6 +18,9 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
         Todo{Name: "Develop sth great"},
     }
 
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+
     if err := json.NewEncoder(w).Encode(todos); err != nil {
         panic(err)
     }
