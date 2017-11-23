2338d92b3727e45ed5d1638a9b0f6c1a35ff0573
Matthias Hertel <me@matthiashertel.eu>
2017-11-23 23:20:54 +0100

    start/stop dev env from scratch with all deps

---
+++ scripts/down_tmux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO stop all started services and running docker containers here
+
+# stop kong container (kong , kong-database, kong-dashboard)
+./scripts/services/kong_compose_down.sh
+./scripts/services/minio_compose_down.sh
+# ./scripts/services/consul_down.sh
+# ./scripts/services/nomad_down.sh
+
+sleep 2;
+
+tmux kill-session -t hpc-rest-dev-env
