fbabb054e040e1f746c6d4a705e06dc1382c0fe3
Matthias Hertel <me@matthiashertel.eu>
2017-11-24 09:40:35 +0100

    test post remove authentication

--- utils/scripts/test_curl_post.sh
+++ utils/scripts/test_curl_post.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
 curl -v -u username:password -H "Content-Type: application/json" -d '{"name":"New Todo "}' http://localhost:8080/todos
+curl -v -d '{"name":"New Todo "}' http://localhost:7777/todos
