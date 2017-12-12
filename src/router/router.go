3e4991d658657568dc58695dcd16f6b864283569
Matthias Hertel <me@matthiashertel.eu>
2017-12-12 11:08:31 +0100

    remove dot import in router.go

--- src/router/router.go
+++ src/router/router.go
@@ -1,23 +1,24 @@
 package router
 
 import (
-	. "github.com/MatthiasHertel/ws17_pp/src/handler"
+	"github.com/MatthiasHertel/ws17_pp/src/handler"
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
 )
 
+// NewRouter init mux router
 func NewRouter() *mux.Router {
 	router := mux.NewRouter()
-	ch := alice.New(LoggingHandler)
+	ch := alice.New(handler.LoggingHandler)
 
 	// ch = ch.Append(BasicAuth)
 
-	router.Handle("/jobs", ch.ThenFunc(AllJobsEndPoint)).Methods("GET")
-	router.Handle("/jobs", ch.ThenFunc(CreateJobEndPoint)).Methods("POST")
-	router.Handle("/jobs", ch.ThenFunc(UpdateJobEndPoint)).Methods("PUT")
-	router.Handle("/jobs", ch.ThenFunc(DeleteJobEndPoint)).Methods("DELETE")
-	router.Handle("/jobs/{id}", ch.ThenFunc(FindJobEndpoint)).Methods("GET")
+	router.Handle("/jobs", ch.ThenFunc(handler.AllJobsEndPoint)).Methods("GET")
+	router.Handle("/jobs", ch.ThenFunc(handler.CreateJobEndPoint)).Methods("POST")
+	router.Handle("/jobs", ch.ThenFunc(handler.UpdateJobEndPoint)).Methods("PUT")
+	router.Handle("/jobs", ch.ThenFunc(handler.DeleteJobEndPoint)).Methods("DELETE")
+	router.Handle("/jobs/{id}", ch.ThenFunc(handler.FindJobEndpoint)).Methods("GET")
 
-	router.Handle("/", ch.ThenFunc(IndexHandler))
+	router.Handle("/", ch.ThenFunc(handler.IndexHandler))
 	return router
 }
