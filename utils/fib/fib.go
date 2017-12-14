a6d31fba8d7101d9783a385f654bf92692c1be6f
Matthias Hertel <m.hertel@cpu-24-7.com>
2017-12-14 17:30:15 +0100

    fib params

--- utils/fib/fib.go
+++ utils/fib/fib.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-  "fmt"
-  "flag"
+	"flag"
+	"log"
 )
-
 
 // intentionally slow
 func fib(n int) int {
-  if n<3 {
-    return 1
-  } else {
-    return fib(n-1)+fib(n-2)
-  }
+	if n < 3 {
+		return 1
+	} else {
+		return fib(n-1) + fib(n-2)
+	}
 }
 
 var n int
 
 func main() {
-  flag.IntVar(&n, "n", 10, "argument for fib")
-  flag.Parse()
+	flag.IntVar(&n, "n", 10, "argument for fib")
+	flag.Parse()
 
-  for i := 1; i <= n; i++ {
-    fmt.Printf("%v => %v\n", i, fib(i))
-  }
+	for i := 1; i <= n; i++ {
+		log.Printf("%v => %v\n", i, fib(i))
+	}
 }
